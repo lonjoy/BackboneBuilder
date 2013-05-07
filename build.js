af02ce8375429b0504280d642fb5766408d373f4
bility <xspider.org@gmail.com>
2013-05-07 15:20:40 +0800

    init

---
+++ build.js
@@ -0,0 +1,22 @@
+var 
+      fs     = require('fs')
+    , Config = require('./config/config.backbone')
+    , Parser = require('./build/parser')
+    , main
+    ;
+
+main = function(){
+    fs.readFile(Config.MainFile,function(err,data){
+        var result;
+        if(err){console.log(err);return;}
+
+        result = new Parser(data.toString());
+        fs.writeFile('pack.js',result.render(),function(err){
+            if(err){console.log(err);return;}
+        });
+       
+        
+    });
+};
+
+main();
