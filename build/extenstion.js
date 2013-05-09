fad2d3720194cd9b24139ba66368121228cc265b
bility <xspider.org@gmail.com>
2013-05-09 17:39:03 +0800

    init

---
+++ build/extenstion.js
@@ -0,0 +1,23 @@
+var Extenstion;
+
+
+Extenstion = function(Parser,context){
+
+};
+
+Extenstion.prototype.Include = function(str){
+
+};
+Extenstion.prototype.IncludeIf = function(condition,str){
+
+};
+Extenstion.prototype.IncludeJoin = function(str,paths){
+
+};
+
+Extenstion.prototype.Push = function(str){
+
+};
+
+
+module.exports = Extenstion;
